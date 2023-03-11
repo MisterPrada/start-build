c51014d062a6f6c7479ca0f23ae6e06744a50c44
Dmitry Shestak <dimonshestak@gmail.com>
2023-03-11 23:21:06 +0300

    Add CLI templates

--- lib/cli/component/gallery-slider/init.php
+++ lib/cli/component/gallery-slider/init.php
@@ -3,12 +3,13 @@
 /**
  * Register block script
  */
+
 add_action( 'init', function() {
 	$template_dir = get_template_directory_uri();
 
 	wp_register_script(
-		'block-{{name}}',
-		$template_dir . hm_assets_bundle( 'js/blocks/block-{{name}}.js' ),
-		['jquery', 'acf']
+		'block-gallery-slider',
+		$template_dir . hm_assets_bundle( 'js/blocks/block-gallery.js' ),
+		array( 'jquery', 'acf' )
 	);
-});
+} );
